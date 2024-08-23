e8f56c5ae3cc7f0321fb36c86d7cb2f45988ebd4
Evan Davis <evandavis427@gmail.com>
2024-08-23 09:19:25 -0600

    us4 complete

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -2,7 +2,7 @@ class ViewingPartiesController < ApplicationController
   def new
     @user = User.find(params[:user_id])
     @movie = MovieFacade.find_movie(params[:movie_id].to_i)
-    @viewing_party = ViewingParty.new
+    @viewing_party = ViewingParty.new(movie_id: @movie.id)
   end
 
   def create
